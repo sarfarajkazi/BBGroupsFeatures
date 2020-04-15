154e0f62ab7cbb3dd9d2d08fcee9d32c2396aa93
Sarfaraj Kazi <sarfarajkazi7@gmail.com>
2020-04-15 19:08:58 +0530

    Solve some structure and remove admins from members list

--- BBGroups-features.php
+++ BBGroups-features.php
@@ -4,6 +4,49 @@ Plugin Name: BBGroups Features
 Plugin URI: http://wordpress.org
 Description: Adds additional features to BuddyBoss Platform groups.
 Version: 1.0
+Tags: buddypress
 Author: Sarfaraj Kazi
 Author URI: http://wordpress.org
+Text Domain: BBGroups-Feature
 */
+/* don't call the file directly */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+define( 'BBGF_FILE', __FILE__ );
+define( 'BBGF_MAIN_PLUGIN', "buddyboss-platform/bp-loader.php" );
+define( 'BBGF_NAME', 'BBGroups Features' );
+/**
+ * Initial plugin
+ */
+/* Only load code that needs BuddyPress to run once BP is loaded and initialized. */
+function check_plugin_with_bb() {
+	require_once 'includes/BBGF_Class.php';
+	BBGF_Class::init();
+}
+
+if ( BBGFValidateDependencies( BBGF_MAIN_PLUGIN ) ) {
+	add_action( 'bp_include', 'check_plugin_with_bb' );
+} else {
+	add_action( 'admin_notices', function () {
+		include_once( dirname( BBGF_FILE ) . '/includes/admin/templates/dependency-error.php' );
+	} );
+}
+/**
+ * Validate Dependence's
+ * checks whether BuddyBoss is active or not.
+ *
+ * @since 1.0.0
+ *
+ * @see __construct relied on
+ * @return boolean if true then it loads the file else it throws dependency error.
+ */
+function BBGFValidateDependencies($name) {
+	$arrActivePlugins = (array) get_option( 'active_plugins', array() );
+	if ( is_multisite() ) {
+		$arrActivePlugins = array_merge( $arrActivePlugins, get_site_option( 'active_sitewide_plugins',array() ) );
+	}
+	return in_array($name, $arrActivePlugins) || array_key_exists( BBGF_MAIN_PLUGIN, $arrActivePlugins );
+}
+
---
+++ includes/admin/templates/dependency-error.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Displays the message of dependency.
+ *
+ * @since 1.0
+ */
+
+echo '<div class="error"><p>';
+if ( ! in_array( BBGF_MAIN_PLUGIN, apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	?>
+    <strong><?php echo BBGF_NAME; ?></strong> - <?php _e( 'plugin requires', 'BBGroups-Feature' ); ?>
+    <a href="https://www.buddyboss.com/platform/"
+       target="_blank">BuddyBoss Platform</a> <?php _e( 'to be installed & activated!', 'wcsm' ); ?>
+	<?php
+}
+if(function_exists('bp_is_active')){
+	if ( ! bp_is_active( 'groups' ) ) {
+		printf(
+			__( '<strong>' . BBGF_NAME . ' </strong> - BuddyBoss Courseware dependency error: <a href="%1$s">%2$s has to be activated</a>!', 'BBGroups-Feature' ),
+			admin_url( 'admin.php?page=bp-components' ),
+			'groups'
+		);
+	}
+}
+
+echo '</p></div>';
+?>
